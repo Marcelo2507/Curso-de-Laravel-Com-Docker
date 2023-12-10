23d75b89992982d8d85c45cfaf74b598e3267cdb
Marcelo2507 <marceloluigi12.p8@gmail.com>
2023-12-10 20:53:48 -0300

    feat: insert a Register

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Support extends Model
 {
     use HasFactory;
+    //afirma quais propriedades podem ser preenchidas e enviadas ao banco;
+    protected $fillable = [
+        'subject',
+        'body',
+        'status'
+    ];
 }
